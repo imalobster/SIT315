a3aca9ec2c5ac9fa849baf10240586c870667016
bastone_deakin <bastone@deakin.edu.au>
2021-08-09 13:58:16 +1000

    added separate files for each case

--- Module2/Seminar2.2P_parallel_add.cpp
+++ Module2/Seminar2.2P_parallel_add.cpp
@@ -7,16 +7,7 @@
 using namespace std::chrono;
 using namespace std;
 
-#define NUM_THREADS 64
-
-// Define struct to hold partition details for each thread completing the number generation task
-struct RngTask
-{
-	int seed;
-	int *v;
-	int start;
-	int end;
-};
+#define NUM_THREADS 8
 
 // Define struct to hold partition details for each thread completing the vector addition task
 struct AddTask
@@ -26,19 +17,13 @@ struct AddTask
 	int end;
 };
 
-// Function that generates and assigns numbers to an array (passed via RngTask struct object)
-void *randomVector(void *args)
+void randomVector(int vector[], int size)
 {
-	// Create a local variable to point to the struct passed in via the args argument
-	RngTask *RngTask = ((struct RngTask *)args);
-	// Initialise the pseudo-random number generator with seed value
-	srand(time(NULL) * RngTask -> seed);
-	// Generate random values between 0 and 99 for the partition 
-	for (int i = RngTask -> start; i < RngTask -> end; i++)
+	for (int i = 0; i < size; i++)
 	{
-		RngTask -> v[i] = rand() % 100;
+		// Generate a random integer between 0 and 99
+		vector[i] = rand() % 100;
 	}
-	return NULL;
 }
 
 // Function that generates and assigns numbers to an array (passed via RngTask struct object)
@@ -53,26 +38,6 @@ void *addVector(void *args)
 		AddTask -> v3[i] = AddTask -> v1[i] + AddTask -> v2[i];
 	}
 	return NULL;
-}
-
-// Handles the creation of and partition of data for each thread used in the number generation task
-void p_assignRandomValuesToVector(int* vector, int seed, int totalSize, int partitionSize, pthread_t arr[])
-{
-	for (size_t i = 0; i < (NUM_THREADS / 2); i++)
-	{
-		// Create new random task struct for each thread to store respective partition data
-		struct RngTask *RngTask = (struct RngTask *)malloc(sizeof(struct RngTask));
-		// Assign the vector pointer of the struct to the specified vector that needs to be populated
-		RngTask -> v = vector;
-		// Specify the seed value and increment it for each thread to generate random values
-		RngTask -> seed = seed++;
-		// Set the start index of the partition the thread will begin generating numbers for
-		RngTask -> start = i * partitionSize;
-		// Set the end index of the partition for the thread, ensuring if it's the last vector it returns the size of the full array
-		RngTask -> end = (i + 1) == (NUM_THREADS / 2) ? totalSize : ((i + 1) * partitionSize);
-		// Create the thread and ask it to compute the randomVector function using the struct prepared above
-		pthread_create(&arr[i], NULL, randomVector, (void *)RngTask);
-	}
 }
 
 // Handles the creation of each thread used in the vector addition task
@@ -110,8 +75,6 @@ int main(){
 
 	int *v1, *v2, *v3;
 
-	// Create thread array for the number generation task
-	pthread_t threads_rngTask[NUM_THREADS];
 	// Create thread array for the addition task
 	pthread_t threads_addTask[NUM_THREADS];
 
@@ -123,21 +86,12 @@ int main(){
 	v2 = (int *) malloc(size * sizeof(int *));
 	v3 = (int *) malloc(size * sizeof(int *));
 
-	// Calculate the partition size per thread needed for number generation task (note divide by two since we want to split threads between v1 and v2)
-	int partitionSize = size / (NUM_THREADS / 2);
-
 	// Use the custom function to generate random values in the first and second blocks of memory
-	p_assignRandomValuesToVector(v1, 0, size, partitionSize, threads_rngTask);
-	p_assignRandomValuesToVector(v2, NUM_THREADS, size, partitionSize, threads_rngTask);
-
-	// Wait for all threads created above to complete
-	for (size_t i = 0; i < NUM_THREADS; i++)
-	{
-		pthread_join(threads_rngTask[i], NULL);
-	}
+	randomVector(v1, size);
+	randomVector(v2, size);
 
 	// Calculate the partition size per thread needed for vector addition task
-	partitionSize = size / NUM_THREADS;
+	int partitionSize = size / NUM_THREADS;
 
 	// Assign values to the third block of memory by adding the values of the respective index location from the first and second blocks
 	p_addVectorByIndex(v1, v2, v3, size, partitionSize, threads_addTask);
--- Module2/Seminar2.2P_parallel_rng.cpp
+++ Module2/Seminar2.2P_parallel_rng.cpp
@@ -7,21 +7,13 @@
 using namespace std::chrono;
 using namespace std;
 
-#define NUM_THREADS 64
+#define NUM_THREADS 8
 
 // Define struct to hold partition details for each thread completing the number generation task
 struct RngTask
 {
 	int seed;
 	int *v;
-	int start;
-	int end;
-};
-
-// Define struct to hold partition details for each thread completing the vector addition task
-struct AddTask
-{
-	int *v1, *v2, *v3;
 	int start;
 	int end;
 };
@@ -37,20 +29,6 @@ void *randomVector(void *args)
 	for (int i = RngTask -> start; i < RngTask -> end; i++)
 	{
 		RngTask -> v[i] = rand() % 100;
-	}
-	return NULL;
-}
-
-// Function that generates and assigns numbers to an array (passed via RngTask struct object)
-void *addVector(void *args)
-{
-	// Create a local variable to point to the struct passed in via the args argument
-	AddTask *AddTask = ((struct AddTask *)args);
-
-	// Assign values to v3 by adding the v1 and v2 values at each respective index
-	for (int i = AddTask -> start; i < AddTask -> end; i++)
-	{
-		AddTask -> v3[i] = AddTask -> v1[i] + AddTask -> v2[i];
 	}
 	return NULL;
 }
@@ -75,34 +53,6 @@ void p_assignRandomValuesToVector(int* vector, int seed, int totalSize, int part
 	}
 }
 
-// Handles the creation of each thread used in the vector addition task
-void p_addVectorByIndex(int* vector1, int* vector2, int* vector3, int totalSize, int partitionSize, pthread_t arr[])
-{
-	for (size_t i = 0; i < NUM_THREADS; i++)
-	{
-		// Create new random task struct for each thread to store respective partition data
-		struct AddTask *AddTask = (struct AddTask *)malloc(sizeof(struct AddTask));
-		// Assign the first vector pointer of the struct
-		AddTask -> v1 = vector1;
-		// Assign the second vector pointer of the struct
-		AddTask -> v2 = vector2;
-		// Assign the thrid vector pointer of the struct (this vector will be the resultant vector)
-		AddTask -> v3 = vector3;
-		// Set the start index to the beginning of the partition
-		AddTask -> start = i * partitionSize;
-		// Set the end index to the end of the partition for the thread, ensuring if it's the last vector it returns the size of the full array
-		AddTask -> end = (i + 1) == NUM_THREADS ? totalSize : ((i + 1) * partitionSize);
-		// Create the thread and ask it to add values from v1 and v2 into v3
-		pthread_create(&arr[i], NULL, addVector, (void *)AddTask);
-	}
-
-	// Wait for all threads created above to complete
-	for (size_t i = 0; i < NUM_THREADS; i++)
-	{
-		pthread_join(arr[i], NULL);
-	}
-}
-
 int main(){
 	unsigned long size = 100000000;
 
@@ -112,8 +62,6 @@ int main(){
 
 	// Create thread array for the number generation task
 	pthread_t threads_rngTask[NUM_THREADS];
-	// Create thread array for the addition task
-	pthread_t threads_addTask[NUM_THREADS];
 
 	// Get the current time before vector assignment
 	auto start = high_resolution_clock::now();
@@ -136,11 +84,11 @@ int main(){
 		pthread_join(threads_rngTask[i], NULL);
 	}
 
-	// Calculate the partition size per thread needed for vector addition task
-	partitionSize = size / NUM_THREADS;
-
 	// Assign values to the third block of memory by adding the values of the respective index location from the first and second blocks
-	p_addVectorByIndex(v1, v2, v3, size, partitionSize, threads_addTask);
+	for (int i = 0; i < size; i++)
+	{
+		v3[i] = v1[i] + v2[i];
+	}
 
 	// Get the current time after vector assignment
 	auto stop = high_resolution_clock::now();
